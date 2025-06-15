dd0b8af9d468f4cc5d2cc4547264cfd8f8f7cc77
Jhonat <jhonatheberson@gmail.com>
2025-06-15 16:13:54 -0300

    feat(UI/UX): ✨ use route statistics in frontend

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Items from './Items';
 import ItemDetail from './ItemDetail';
 import ItemForm from './ItemForm';
+import Stats from './Stats';
 import { DataProvider } from '../state/DataContext';
 
 function App() {
@@ -11,11 +12,13 @@ function App() {
       <nav style={{ padding: 16, borderBottom: '1px solid #ddd' }}>
         <Link to="/">Items</Link>
         <Link to="/items/new" style={{ marginLeft: 16 }}>Add Item</Link>
+        <Link to="/stats" style={{ marginLeft: 16 }}>Statistics</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Items />} />
         <Route path="/items/:id" element={<ItemDetail />} />
         <Route path="/items/new" element={<ItemForm />} />
+        <Route path="/stats" element={<Stats />} />
       </Routes>
     </DataProvider>
   );
